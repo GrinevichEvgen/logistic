ee3fd16b9e7c78572f578b27ec84b3cc418cef53
GrinevichEvgen <grinevichevgen@gmail.com>
2023-07-10 15:01:23 +0300

    add app drivers

--- drivers/models.py
+++ drivers/models.py
@@ -5,5 +5,5 @@ class Driver(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
 
-    def str(self):
+    def __str__(self):
         return f"{self.first_name} {self.last_name}"
